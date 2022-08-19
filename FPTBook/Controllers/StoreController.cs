ac4989afc43c7e7ccfdc7cf32924e02e03a5e783
canh luong duc <canhldgcd18745@fpt.edu.vn>
2022-08-19 08:37:33 +0700

    Create StoreController

---
+++ FPTBook/Controllers/StoreController.cs
@@ -0,0 +1,20 @@
+﻿using FPTBook.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace FPTBook.Controllers
+{
+    public class StoreController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public StoreController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Books.ToListAsync());
+        }
+    }
+}
